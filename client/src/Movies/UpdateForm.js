ad5cdc16144f9445ceebb63eb15c9860861a5a9a
dominique maack <dominiquemaack@gmail.com>
2019-10-17 15:49:16 -0600

    put request is working but onsubmit is throwing an error

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -2,31 +2,53 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-   const initialMovie = {
-       id: '',
-       title: '',
-       director: '',
-       metascore: '',
-       stars: ''
-   }
+ 
 
 
 const UpdateForm = props => {
     console.log('Update Form props', props)
+
+    const initialMovie = {
+        id: '',
+        title: '',
+        director: '',
+        metascore: '',
+        stars: []
+    }
+
+
     const [movie, setMovie] = useState(initialMovie)
     console.log(movie);
 
-    const changeHandler = e => {
-        return null
+    useEffect(() => {
+
+        const id = props.match.params.id;
+        const movieToEdit = props.movies.find(movie => `${movie.id}` === id)
+        if(movieToEdit) {
+            setMovie(movieToEdit)
+        };
+    }, [props.movies, props.match.params]) 
+
+   
+
+    const changeHandler = ev => {
+        ev.persist();
+        let value = ev.target.value;
+        setMovie({
+            ...movie,
+            [ev.target.name]: value
+        })
     }
 
     const handleSubmit = e => {
-        e.preventdefault();
+        e.preventDefault();
         axios
-        .put(`http://localhost5000/api/movies/${props.match.params.id}`, movie)
+        .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
         .then(res => {
             console.log('response from axios PUT request', res)
             props.setMovies(...props.movies, res.data)
+            // setMovie(initialMovie)
+            props.history.push(`/movie-list/`)
         })
         .catch(err => {
             console.log('No data', err)
@@ -40,30 +62,30 @@ const UpdateForm = props => {
                 <input
                     type='text'
                     name='title'
-                    placeholder='Enter new title'
-                    // onChange={}
-                    // value={}
+                    placeholder='title'
+                    onChange={changeHandler}
+                    value={movie.title}
                 />
                 <input
                     type='text'
                     name='director'
-                    placeholder='Enter new director'
-                    // onChange={}
-                    // value={}
+                    placeholder='director'
+                    onChange={changeHandler}
+                    value={movie.director}
                 />
                 <input
                     type='text'
                     name='metascore'
-                    placeholder=''
-                    // onChange={}
-                    // value={}
+                    placeholder='metascore'
+                    onChange={changeHandler}
+                    value={movie.metascore}
                 />
                 <input
                     type='text'
                     name='stars'
-                    placeholder=''
-                    // onChange={}
-                    // value={}
+                    placeholder='stars'
+                    onChange={changeHandler}
+                    value={movie.stars}
                 />
                 <button type='submit'>Update</button>
             </form>
